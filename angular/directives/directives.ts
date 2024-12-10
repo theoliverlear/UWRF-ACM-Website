8536286b5b10ab3f524cd80ebf44bed88bc56416
theoliverlear <sigwartholiver2@gmail.com>
2024-12-10 15:17:53 -0600

    Add RumbleDirective to directives.ts to inject new rumbling element functionality

--- angular/directives/directives.ts
+++ angular/directives/directives.ts
@@ -1,7 +1,9 @@
 import {UndraggableDirective} from "./undraggable.directive";
 import {UnoptimizedImageDirective} from "./unoptimized-image.directive";
+import {RumbleDirective} from "./rumble.directive";
 
 export const directives = [
+    RumbleDirective,
     UndraggableDirective,
     UnoptimizedImageDirective
 ]
