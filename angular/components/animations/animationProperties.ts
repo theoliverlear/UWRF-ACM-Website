51a528be6ba14bd2aa48add1f4e031382593518a
theoliverlear <sigwartholiver2@gmail.com>
2024-12-10 15:16:19 -0600

    Add animationProperties.ts to define animation properties for front-end

---
+++ angular/components/animations/animationProperties.ts
@@ -0,0 +1,7 @@
+import {FadeInOutProperties} from "./models/FadeInOutProperties";
+
+export const acmTitleFadeInOutAnimationProperties: FadeInOutProperties = {
+    fadeInDuration: '600ms',
+    waitDuration: '3000ms',
+    fadeOutDuration: '600ms'
+};
