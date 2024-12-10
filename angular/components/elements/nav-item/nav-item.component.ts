fdc75dd11dcd92fcedbd48e61ec0330ebd917b4b
theoliverlear <sigwartholiver2@gmail.com>
2024-12-10 11:10:47 -0600

    Add inputs and click handling linked to SsAnchorComponent in nav-item.component.ts

--- angular/components/elements/nav-item/nav-item.component.ts
+++ angular/components/elements/nav-item/nav-item.component.ts
@@ -1,5 +1,7 @@
 // nav-item.component.ts 
-import { Component } from "@angular/core";
+import {Component, HostListener, Input, ViewChild} from "@angular/core";
+import { TextElementLink } from "../../../models/link/TextElementLink";
+import {SsAnchorComponent} from "../ss-anchor/ss-anchor.component";
 
 @Component({
     selector: 'nav-item',
@@ -7,7 +9,13 @@ import { Component } from "@angular/core";
     styleUrls: ['./nav-item-style.component.css']
 })
 export class NavItemComponent {
+    @Input() elementLink: TextElementLink;
+    @ViewChild(SsAnchorComponent) anchorComponent: SsAnchorComponent;
     constructor() {
         
+    }
+    @HostListener('click')
+    onClick() {
+        this.anchorComponent.onClick();
     }
 }
