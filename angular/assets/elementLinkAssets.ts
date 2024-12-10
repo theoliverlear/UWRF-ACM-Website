1c2dd2fe4178852993d9163fa76c6dd3643d26c5
theoliverlear <sigwartholiver2@gmail.com>
2024-12-10 11:03:45 -0600

    Update elementLinkAssets.ts by adding new navigation links and switch navBarHomeElementLink to ElementLink

--- angular/assets/elementLinkAssets.ts
+++ angular/assets/elementLinkAssets.ts
@@ -1,9 +1,27 @@
 import {TextElementLink} from "../models/link/TextElementLink";
 import {TargetType} from "../models/html/TargetType";
+import {ElementLink} from "../models/link/ElementLink";
+import {TagType} from "../models/html/TagType";
 
-export const navBarHomeElementLink = new TextElementLink('/',
-                                                         TargetType.SELF);
-export const navBarSearchElementLink = new TextElementLink('/search',
-                                                           TargetType.SELF);
-export const navBarAccountElementLink = new TextElementLink('/authorize',
-                                                            TargetType.SELF);
+export const navBarHomeElementLink = new ElementLink('/',
+                                                     TargetType.SELF);
+export const navBarJoinElementLink = new TextElementLink('/join',
+                                                         TargetType.SELF,
+                                                         false,
+                                                         'Join',
+                                                         TagType.H4);
+export const navBarAboutElementLink = new TextElementLink('/about',
+                                                          TargetType.SELF,
+                                                          false,
+                                                          'About',
+                                                          TagType.H4);
+export const navBarThisWeekElementLink = new TextElementLink('/this-week',
+                                                             TargetType.SELF,
+                                                             false,
+                                                             'This Week',
+                                                             TagType.H4);
+export const navBarDevTeamElementLink = new TextElementLink('/dev-team',
+                                                            TargetType.SELF,
+                                                            false,
+                                                            'Dev Team',
+                                                            TagType.H4);
