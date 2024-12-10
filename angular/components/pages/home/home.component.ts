549482141a5d7fd8c4dda3656e69f730153d3cc6
theoliverlear <sigwartholiver2@gmail.com>
2024-12-10 15:22:36 -0600

    Add promo sequence logic and animation integration in home.component.ts

--- angular/components/pages/home/home.component.ts
+++ angular/components/pages/home/home.component.ts
@@ -1,13 +1,56 @@
 // home.component.ts 
-import { Component } from "@angular/core";
+import {AfterViewInit, Component, ViewChild} from "@angular/core";
+import {TypeSpeed} from "../../elements/typing-text/models/TypeSpeed";
+import {
+    promoTextAcronym,
+    promoTextCallUs,
+    promoTextFullTitle
+} from "../../../assets/textAssets";
+import {TagType} from "../../../models/html/TagType";
+import {
+    TypingTextComponent
+} from "../../elements/typing-text/typing-text.component";
+import {
+    acmTitleFadeInOutAnimationProperties
+} from "../../animations/animationProperties";
 
 @Component({
     selector: 'home',
     templateUrl: './home.component.html',
     styleUrls: ['./home-style.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit{
+    @ViewChild('topTypingText') topTypingText: TypingTextComponent;
+    showFadeInOut: boolean = false;
     constructor() {
         
     }
+
+    ngAfterViewInit(): void {
+        this.promoSequence();
+    }
+    promoSequence(): Promise<void> {
+        this.topTypingText.typeText().then(() => {
+            this.topTypingText.booleanBlinkingCursor = true; // Start blinking
+            this.topTypingText.toggleBlinkingClass();
+
+            this.topTypingText.delay(2000).then(() => {
+                this.topTypingText.booleanBlinkingCursor = false; // Stop blinking during deletion
+                this.topTypingText.deleteText().then(() => {
+                    this.topTypingText.textToType = promoTextCallUs;
+                    this.topTypingText.typeText().then(() => {
+                        this.topTypingText.booleanBlinkingCursor = true; // Resume blinking after new text is typed
+                            this.showFadeInOut = true;
+                    });
+                });
+            });
+        });
+        return Promise.resolve();
+    }
+
+    protected readonly TypeSpeed = TypeSpeed;
+    protected readonly promoTextFullTitle = promoTextFullTitle;
+    protected readonly TagType = TagType;
+    protected readonly promoTextAcronym = promoTextAcronym;
+    protected readonly acmTitleFadeInOutAnimationProperties = acmTitleFadeInOutAnimationProperties;
 }
