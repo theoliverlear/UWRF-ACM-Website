6a38f038a45acd2332924203c219f333f67f3a5a
theoliverlear <sigwartholiver2@gmail.com>
2024-12-10 15:23:21 -0600

    Add fade-in-out-content.component.ts to be an Angular component for the fade-in-out-content element in the Angular application

---
+++ angular/components/elements/fade-in-out-content/fade-in-out-content.component.ts
@@ -0,0 +1,31 @@
+// fade-in-out-content.component.ts 
+import {Component, HostBinding, Input} from "@angular/core";
+import {fadeInOut} from "../../animations/animations";
+import {
+    FadeInOutProperties
+} from "../../animations/models/FadeInOutProperties";
+
+@Component({
+    selector: 'fade-in-out-content',
+    templateUrl: './fade-in-out-content.component.html',
+    styleUrls: ['./fade-in-out-content-style.component.css'],
+    animations: [
+        fadeInOut
+    ]
+})
+export class FadeInOutContentComponent {
+    @Input() fadeInOutAnimationProperties: FadeInOutProperties;
+    @HostBinding('@fadeInOut') get fadeInOut() {
+        return {
+            value: '',
+            params: {
+                fadeInDuration: this.fadeInOutAnimationProperties.fadeInDuration,
+                waitDuration: this.fadeInOutAnimationProperties.waitDuration,
+                fadeOutDuration: this.fadeInOutAnimationProperties.fadeOutDuration
+            }
+        }
+    }
+    constructor() {
+        
+    }
+}
