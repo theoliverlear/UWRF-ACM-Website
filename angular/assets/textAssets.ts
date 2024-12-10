6f8a251c3faac1d625b3fa47e9fbdb73271e0987
theoliverlear <sigwartholiver2@gmail.com>
2024-12-10 15:19:40 -0600

    Add promotional text constants in textAssets.ts for ACM branding

---
+++ angular/assets/textAssets.ts
@@ -0,0 +1,3 @@
+export const promoTextFullTitle: string = "We're the Association of Computing Machinery.";
+export const promoTextCallUs: string = "You can call us..."
+export const promoTextAcronym: string = "ACM"
