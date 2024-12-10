1b5c8f81cb486e21c4da9d9bd02ad2a030a3d6ff
theoliverlear <sigwartholiver2@gmail.com>
2024-12-10 11:19:51 -0600

    Add ss-title.component.ts to be an Angular component for the ss-title element in the Angular application

---
+++ angular/components/elements/ss-title/ss-title.component.ts
@@ -0,0 +1,13 @@
+// ss-title.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'ss-title',
+    templateUrl: './ss-title.component.html',
+    styleUrls: ['./ss-title-style.component.css']
+})
+export class SsTitleComponent {
+    constructor() {
+        
+    }
+}
