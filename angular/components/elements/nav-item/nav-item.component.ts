b006b41a308a1bb55ea7afcb23a42f59485279b1
theoliverlear <sigwartholiver2@gmail.com>
2024-12-09 21:23:02 -0600

    Add nav-item.component.ts for creating navigation item elements in Angular application

---
+++ angular/components/elements/nav-item/nav-item.component.ts
@@ -0,0 +1,13 @@
+// nav-item.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'nav-item',
+    templateUrl: './nav-item.component.html',
+    styleUrls: ['./nav-item-style.component.css']
+})
+export class NavItemComponent {
+    constructor() {
+        
+    }
+}
