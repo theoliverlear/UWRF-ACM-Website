615ea68cb08f87bf20af3c718fb76adfe66eed76
theoliverlear <sigwartholiver2@gmail.com>
2024-12-11 21:11:25 -0600

    Add about.component.ts to be an Angular component for the about element in the Angular application

---
+++ angular/components/pages/about/about.component.ts
@@ -0,0 +1,13 @@
+// about.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about-style.component.css']
+})
+export class AboutComponent {
+    constructor() {
+        
+    }
+}
