d77ed3154e9fb1a137ce38e1ffb7459449a7a8ef
theoliverlear <sigwartholiver2@gmail.com>
2024-12-11 21:06:41 -0600

    Add fade-in-content.component.ts to be an Angular component for the fade-in-content element in the Angular application

---
+++ angular/components/elements/fade-in-content/fade-in-content.component.ts
@@ -0,0 +1,13 @@
+// fade-in-content.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'fade-in-content',
+    templateUrl: './fade-in-content.component.html',
+    styleUrls: ['./fade-in-content-style.component.css']
+})
+export class FadeInContentComponent {
+    constructor() {
+        
+    }
+}
