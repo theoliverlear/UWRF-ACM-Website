1e76f79dc612828af0fcc844f82f4021192409f0
theoliverlear <sigwartholiver2@gmail.com>
2024-12-11 20:46:01 -0600

    Add scale up animation for ACM title in animationProperties.ts

--- angular/components/animations/animationProperties.ts
+++ angular/components/animations/animationProperties.ts
@@ -1,7 +1,13 @@
 import {FadeInOutProperties} from "./models/FadeInOutProperties";
+import {ScaleProperties} from "./models/ScaleProperties";
 
 export const acmTitleFadeInOutAnimationProperties: FadeInOutProperties = {
     fadeInDuration: '600ms',
-    waitDuration: '3000ms',
+    waitDuration: '1500ms',
     fadeOutDuration: '600ms'
+};
+export const acmTitleScaleAnimationProperties: ScaleProperties = {
+    scaleUpDuration: '600ms',
+    waitDuration: '1500ms',
+    scaleAmount: 'scale(1.5)'
 };
