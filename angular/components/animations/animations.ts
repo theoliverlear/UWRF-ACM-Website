251827aa5bc1c17d0f35cc6851405cb788d3840f
theoliverlear <sigwartholiver2@gmail.com>
2024-12-11 20:46:44 -0600

    Add fadeInAnimation and rename fadeInOut to fadeInOutAnimation in animations.ts to improve animation variable namespace clarity

--- angular/components/animations/animations.ts
+++ angular/components/animations/animations.ts
@@ -6,12 +6,18 @@ import {
     trigger
 } from "@angular/animations";
 
-export const fadeInOut: AnimationTriggerMetadata = trigger('fadeInOut', [
+export const fadeInOutAnimation: AnimationTriggerMetadata = trigger('fadeInOut', [
     transition(':enter', [
         style({opacity: 0}),
         animate('{{ fadeInDuration }} {{ waitDuration }}', style({opacity: 1}))
     ]),
     transition(':leave', [
         animate('{{ fadeOutDuration }} {{ waitDuration }}', style({opacity: 0}))
+    ])
+]);
+export const fadeInAnimation: AnimationTriggerMetadata = trigger('fadeIn', [
+    transition(':enter', [
+        style({ opacity: 0 }),
+        animate('{{ duration }} {{ delay }}', style({ opacity: 1 }))
     ])
 ]);
