b18924dfeb818c9db74b554b7732d8f40ca1f0d1
theoliverlear <sigwartholiver2@gmail.com>
2024-12-12 14:30:17 -0600

    Add floating-content.component.ts to be an Angular component for the floating-content element in the Angular application

---
+++ angular/components/elements/floating-content/floating-content.component.ts
@@ -0,0 +1,14 @@
+// floating-content.component.ts 
+import {AfterViewInit, Component, ElementRef, ViewChild} from "@angular/core";
+import {gsap} from "gsap";
+@Component({
+    selector: 'floating-content',
+    templateUrl: './floating-content.component.html',
+    styleUrls: ['./floating-content-style.component.css']
+})
+export class FloatingContentComponent {
+    constructor() {
+        
+    }
+
+}
