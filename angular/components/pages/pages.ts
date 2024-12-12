b1a2160833f1bea0d80f140d0abf14b239b5aa99
theoliverlear <sigwartholiver2@gmail.com>
2024-12-11 21:00:20 -0600

    Add new webpage components to the pages array in pages.ts

--- angular/components/pages/pages.ts
+++ angular/components/pages/pages.ts
@@ -1,5 +1,15 @@
 import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
+import {DevTeamComponent} from "./dev-team/dev-team.component";
+import {JoinComponent} from "./join/join.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
+import {ThisWeekComponent} from "./this-week/this-week.component";
 
-export const pages: any[] = [
-    HomeComponent
+export const pages = [
+    AboutComponent,
+    DevTeamComponent,
+    HomeComponent,
+    JoinComponent,
+    NotFoundComponent,
+    ThisWeekComponent
 ]
