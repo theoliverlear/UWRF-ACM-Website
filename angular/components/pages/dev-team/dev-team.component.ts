d4ca33778455031fd24db89cbc55aa61a5c980b7
theoliverlear <sigwartholiver2@gmail.com>
2024-12-11 21:12:03 -0600

    Add dev-team.component.ts to be an Angular component for the dev-team element in the Angular application

---
+++ angular/components/pages/dev-team/dev-team.component.ts
@@ -0,0 +1,13 @@
+// dev-team.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'dev-team',
+    templateUrl: './dev-team.component.html',
+    styleUrls: ['./dev-team-style.component.css']
+})
+export class DevTeamComponent {
+    constructor() {
+        
+    }
+}
