5e858b840ac69d1c1d372c71957032863bdaaf42
theoliverlear <sigwartholiver2@gmail.com>
2024-12-12 14:29:50 -0600

    Add fade-in animation with configurable properties in fade-in-content.component.ts

--- angular/components/elements/fade-in-content/fade-in-content.component.ts
+++ angular/components/elements/fade-in-content/fade-in-content.component.ts
@@ -1,12 +1,30 @@
 // fade-in-content.component.ts 
-import { Component } from "@angular/core";
+import {Component, HostBinding, Input} from "@angular/core";
+import {FadeInProperties} from "../../animations/models/FadeInProperties";
+import {
+    defaultFadeInAnimationProperties
+} from "../../animations/animationProperties";
+import {fadeInAnimation} from "../../animations/animations";
 
 @Component({
     selector: 'fade-in-content',
     templateUrl: './fade-in-content.component.html',
-    styleUrls: ['./fade-in-content-style.component.css']
+    styleUrls: ['./fade-in-content-style.component.css'],
+    animations: [
+        fadeInAnimation
+    ]
 })
 export class FadeInContentComponent {
+    @Input() animationProperties: FadeInProperties = defaultFadeInAnimationProperties;
+    @HostBinding('@fadeIn') get fadeIn() {
+        return {
+            value: '',
+            params: {
+                duration: this.animationProperties.duration,
+                delay: this.animationProperties.delay
+            }
+        }
+    }
     constructor() {
         
     }
