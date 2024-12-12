d355086c28ba2e94a4d54ca6e0cbfda61b99851f
theoliverlear <sigwartholiver2@gmail.com>
2024-12-11 21:12:39 -0600

    Add join.component.ts to be an Angular component for the join element in the Angular application

---
+++ angular/components/pages/join/join.component.ts
@@ -0,0 +1,13 @@
+// join.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'join',
+    templateUrl: './join.component.html',
+    styleUrls: ['./join-style.component.css']
+})
+export class JoinComponent {
+    constructor() {
+        
+    }
+}
