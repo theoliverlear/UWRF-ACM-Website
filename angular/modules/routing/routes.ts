d10fb9523231268c002330b17f04ef859511db34
theoliverlear <sigwartholiver2@gmail.com>
2024-12-11 20:53:59 -0600

    Add new routes for About, Dev Team, Join, This Week, and Not Found pages in routes.ts

--- angular/modules/routing/routes.ts
+++ angular/modules/routing/routes.ts
@@ -1,6 +1,34 @@
 import {Route} from "@angular/router";
 import {HomeComponent} from "../../components/pages/home/home.component";
-
+import {
+    NotFoundComponent
+} from "../../components/pages/not-found/not-found.component";
+import {JoinComponent} from "../../components/pages/join/join.component";
+import {
+    DevTeamComponent
+} from "../../components/pages/dev-team/dev-team.component";
+import {
+    ThisWeekComponent
+} from "../../components/pages/this-week/this-week.component";
+import {AboutComponent} from "../../components/pages/about/about.component";
+export const aboutRoute: Route = {
+    path: 'about',
+    component: AboutComponent,
+    data: {
+        meta: {
+            title: 'About | UWRF ACM'
+        }
+    }
+};
+export const devTeamRoute: Route = {
+    path: 'dev-team',
+    component: DevTeamComponent,
+    data: {
+        meta: {
+            title: 'Dev Team | UWRF ACM'
+        }
+    }
+};
 export const homeRoute: Route = {
     path: '',
     component: HomeComponent,
@@ -9,8 +37,39 @@ export const homeRoute: Route = {
             title: 'UWRF Association of Computing Machinery',
         },
     }
-}
-
+};
+export const joinRoute: Route = {
+    path: 'join',
+    component: JoinComponent,
+    data: {
+        meta: {
+            title: 'Join | UWRF ACM'
+        }
+    }
+};
+export const thisWeekRoute: Route = {
+    path: 'this-week',
+    component: ThisWeekComponent,
+    data: {
+        meta: {
+            title: 'This Week | UWRF ACM'
+        }
+    }
+};
+export const notFoundRoute: Route = {
+    path: '**',
+    component: NotFoundComponent,
+    data: {
+        meta: {
+            title: 'Page Not Found'
+        }
+    }
+};
 export const routes: Route[] = [
-    homeRoute
+    aboutRoute,
+    devTeamRoute,
+    homeRoute,
+    joinRoute,
+    thisWeekRoute,
+    notFoundRoute
 ];
