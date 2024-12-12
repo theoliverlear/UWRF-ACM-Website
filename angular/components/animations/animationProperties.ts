7b88d7665522e5aa8e53c73ff782661cda895652
theoliverlear <sigwartholiver2@gmail.com>
2024-12-12 14:25:21 -0600

    Add default and component-specific fade-in animation properties in animationProperties.ts

--- angular/components/animations/animationProperties.ts
+++ angular/components/animations/animationProperties.ts
@@ -1,5 +1,6 @@
 import {FadeInOutProperties} from "./models/FadeInOutProperties";
 import {ScaleProperties} from "./models/ScaleProperties";
+import {FadeInProperties} from "./models/FadeInProperties";
 
 export const acmTitleFadeInOutAnimationProperties: FadeInOutProperties = {
     fadeInDuration: '600ms',
@@ -10,4 +11,20 @@ export const acmTitleScaleAnimationProperties: ScaleProperties = {
     scaleUpDuration: '600ms',
     waitDuration: '1500ms',
     scaleAmount: 'scale(1.5)'
+};
+export const defaultFadeInAnimationProperties: FadeInProperties = {
+    duration: '300ms',
+    delay: '0ms'
+};
+export const thisWeekTitleFadeInAnimationProperties: FadeInProperties = {
+    duration: '600ms',
+    delay: '250ms'
+};
+export const thisWeekSubtitleFadeInAnimationProperties: FadeInProperties = {
+    duration: '600ms',
+    delay: '1500ms'
+};
+export const homeEventPopupFadeInAnimationProperties: FadeInProperties = {
+    duration: '200ms',
+    delay: '50ms'
 };
