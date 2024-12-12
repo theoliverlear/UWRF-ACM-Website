d92f7f05c7c627b49cb86a7168252adafd49fc84
theoliverlear <sigwartholiver2@gmail.com>
2024-12-11 21:13:45 -0600

    Add not-found.component.ts to be an Angular component for the not-found element in the Angular application

---
+++ angular/components/pages/not-found/not-found.component.ts
@@ -0,0 +1,16 @@
+// not-found.component.ts 
+import { Component } from "@angular/core";
+import {TagType} from "../../../models/html/TagType";
+
+@Component({
+    selector: 'not-found',
+    templateUrl: './not-found.component.html',
+    styleUrls: ['./not-found-style.component.css']
+})
+export class NotFoundComponent {
+    constructor() {
+        
+    }
+
+    protected readonly TagType = TagType;
+}
