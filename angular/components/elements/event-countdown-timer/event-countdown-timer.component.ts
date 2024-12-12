f1857ee9ed243c3860b3fb9bbed8f342f1aa2cd2
theoliverlear <sigwartholiver2@gmail.com>
2024-12-11 21:05:13 -0600

    Add event-countdown-timer.component.ts to be an Angular component for the event-countdown-timer element in the Angular application

---
+++ angular/components/elements/event-countdown-timer/event-countdown-timer.component.ts
@@ -0,0 +1,27 @@
+// event-countdown-timer.component.ts 
+import {Component, Input, OnInit} from "@angular/core";
+import {DateInterval} from "./models/DateInterval";
+
+@Component({
+    selector: 'event-countdown-timer',
+    templateUrl: './event-countdown-timer.component.html',
+    styleUrls: ['./event-countdown-timer-style.component.css']
+})
+export class EventCountdownTimerComponent implements OnInit {
+    @Input() eventDate: Date;
+    remainingTime: number;
+    constructor() {
+        
+    }
+    ngOnInit() {
+        this.updateTime();
+        setInterval(() => this.updateTime(), 1000);
+    }
+    updateTime() {
+        const currentTime: number = new Date().getTime();
+        const targetTime: number = this.eventDate.getTime();
+        const remainingTime: number = (targetTime - currentTime) / 1000;
+        this.remainingTime = remainingTime;
+    }
+    protected readonly DateInterval = DateInterval;
+}
