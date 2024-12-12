b8e3dbd7216e3f5c468d93c284aca3451b99fb9a
theoliverlear <sigwartholiver2@gmail.com>
2024-12-11 21:05:49 -0600

    Add event-popup.component.ts to be an Angular component for the event-popup element in the Angular application

---
+++ angular/components/elements/event-popup/event-popup.component.ts
@@ -0,0 +1,16 @@
+// event-popup.component.ts 
+import {Component, Input} from "@angular/core";
+import {MeetingEvent} from "./models/MeetingEvent";
+import {nextEvent} from "../../../assets/eventAssets";
+
+@Component({
+    selector: 'event-popup',
+    templateUrl: './event-popup.component.html',
+    styleUrls: ['./event-popup-style.component.css']
+})
+export class EventPopupComponent {
+    @Input() meetingEvent: MeetingEvent = nextEvent;
+    constructor() {
+        
+    }
+}
