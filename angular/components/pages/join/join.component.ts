0ec187283e1d0b558bf4efc7f22ec38f164c8d67
theoliverlear <sigwartholiver2@gmail.com>
2024-12-17 11:33:36 -0600

    Add imports and class properties for Discord and in-person join page assets in join.component.ts

--- angular/components/pages/join/join.component.ts
+++ angular/components/pages/join/join.component.ts
@@ -1,5 +1,12 @@
 // join.component.ts 
 import { Component } from "@angular/core";
+import {TagType} from "../../../models/html/TagType";
+import {discordLinkImageAsset} from "../../../assets/imageAssets";
+import {
+    discordJoinLinkText,
+    discordTitleText, inPersonDetailsText, inPersonTitleText
+} from "../../../assets/joinAssets";
+import {discordElementLink} from "../../../assets/elementLinkAssets";
 
 @Component({
     selector: 'join',
@@ -10,4 +17,12 @@ export class JoinComponent {
     constructor() {
         
     }
+
+    protected readonly TagType = TagType;
+    protected readonly discordLinkImageAsset = discordLinkImageAsset;
+    protected readonly discordTitleText = discordTitleText;
+    protected readonly discordJoinLinkText = discordJoinLinkText;
+    protected readonly inPersonTitleText = inPersonTitleText;
+    protected readonly inPersonDetailsText = inPersonDetailsText;
+    protected readonly discordElementLink = discordElementLink;
 }
