d2dbccce33beac9c5b183204464a870a8bc61e02
theoliverlear <sigwartholiver2@gmail.com>
2024-12-17 07:39:58 -0600

    Add ss-paragraph.component.ts to be an Angular component for the ss-paragraph element in the Angular application

---
+++ angular/components/elements/ss-paragraph/ss-paragraph.component.ts
@@ -0,0 +1,14 @@
+// ss-paragraph.component.ts 
+import {Component, Input} from "@angular/core";
+
+@Component({
+    selector: 'ss-paragraph',
+    templateUrl: './ss-paragraph.component.html',
+    styleUrls: ['./ss-paragraph-style.component.css']
+})
+export class SsParagraphComponent {
+    @Input() text: string;
+    constructor() {
+        
+    }
+}
