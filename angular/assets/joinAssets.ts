6f2df257aabdc43094f3386ae013e5ac81dfdb04
theoliverlear <sigwartholiver2@gmail.com>
2024-12-17 11:29:33 -0600

    Add joinAssets.ts to store assets for the join webpage

---
+++ angular/assets/joinAssets.ts
@@ -0,0 +1,6 @@
+export const discordTitleText: string = 'Discord is our community hub!';
+export const discordJoinLinkText: string = 'Click or scan the QR code to join!';
+export const inPersonTitleText: string = 'We do our meetings in South Hall 21.';
+export const inPersonDetailsText:  string = 'ACM meets every Thursday at 4:30' +
+    ' to 6:00 PM in-person and in Discord. Dev Team meets 4:30 to 6:30 PM ' +
+    'on Tuesdays in-person and in Discord.';
