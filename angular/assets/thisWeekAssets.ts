6a8963117bd1ac748b335bbb71391599104eed0d
theoliverlear <sigwartholiver2@gmail.com>
2024-12-17 11:30:35 -0600

    Add logic to handle no-meeting scenario in thisWeekAssets.ts

--- angular/assets/thisWeekAssets.ts
+++ angular/assets/thisWeekAssets.ts
@@ -1,9 +1,14 @@
 export const firstMeetingTitle: [string, Date] = ["Say hey, hi, hello, welcome!",
                                                   new Date('2025-02-07')];
+export const noMeetingTitle: [string, Date] = ["Nothing, we are not meeting this week.",
+                                                  new Date()];
 export const eventTitles: [string, Date][] = [
     firstMeetingTitle
 ];
 function getNextMeetingTitle(): [string, Date] {
+    if (!hasMeetingThisWeek()) {
+        return noMeetingTitle;
+    }
     const now = new Date();
     const upcomingEvents: [string, Date][] = eventTitles.filter((event: [string, Date]): boolean => {
         return event[1] > now;
@@ -16,5 +21,16 @@ function getNextMeetingTitle(): [string, Date] {
     } else {
         return null;
     }
+}
+function hasMeetingThisWeek(): boolean {
+    const oneWeekInMilliseconds = 7 * 24 * 60 * 60 * 1000;
+    const today = new Date().getTime();
+    for (const event of eventTitles) {
+        const eventDate = event[1].getTime();
+        if (eventDate > today && eventDate <= today + oneWeekInMilliseconds) {
+            return true;
+        }
+    }
+    return false;
 }
 export const nextMeetingTitle: [string, Date] = getNextMeetingTitle();
