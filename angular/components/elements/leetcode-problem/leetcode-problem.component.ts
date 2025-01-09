3584465afe1b8c73758da66688c211260d048200
theoliverlear <sigwartholiver2@gmail.com>
2025-01-09 10:14:58 -0600

    Add leetcode-problem.component.ts to be an Angular component for the leetcode-problem element in the Angular application

---
+++ angular/components/elements/leetcode-problem/leetcode-problem.component.ts
@@ -0,0 +1,51 @@
+// leetcode-problem.component.ts
+import {Component, Input, OnInit} from "@angular/core";
+import {LeetCodeProblem} from "../../../models/leetcode/LeetCodeProblem";
+import {TextElementLink} from "../../../models/link/TextElementLink";
+import {TargetType} from "../../../models/html/TargetType";
+import {TagType} from "../../../models/html/TagType";
+import {ProblemDifficulty} from "../../../models/leetcode/ProblemDifficulty";
+
+@Component({
+    selector: 'leetcode-problem',
+    templateUrl: './leetcode-problem.component.html',
+    styleUrls: ['./leetcode-problem-style.component.css']
+})
+export class LeetCodeProblemComponent implements OnInit {
+    @Input() problem: LeetCodeProblem;
+    public elementLink: TextElementLink;
+    constructor() {
+
+    }
+    ngOnInit() {
+        this.elementLink = new TextElementLink(this.problem.link,
+                                               TargetType.BLANK,
+                                               false,
+                                               this.problem.title,
+                                               TagType.H3);
+    }
+    public hasHint(): boolean {
+        const isUndefined: boolean = this.problem.hint === undefined;
+        const isEmpty: boolean = this.problem.hint === "";
+        const isNull: boolean = this.problem.hint === null;
+        const hasHint: boolean = !(isUndefined || isEmpty || isNull);
+        return hasHint;
+    }
+    public getDifficultyClass() {
+        let difficultyClass: string;
+        switch (this.problem.difficulty) {
+            case ProblemDifficulty.EASY:
+                difficultyClass = "easy";
+                break;
+            case ProblemDifficulty.MEDIUM:
+                difficultyClass = "medium";
+                break;
+            case ProblemDifficulty.HARD:
+                difficultyClass = "hard";
+                break;
+            default:
+                difficultyClass = "easy";
+        }
+        return difficultyClass;
+    }
+}
