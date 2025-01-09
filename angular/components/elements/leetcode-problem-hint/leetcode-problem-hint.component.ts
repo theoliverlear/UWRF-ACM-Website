99ab89592274d49d2491d6a15e86a1667d317c04
theoliverlear <sigwartholiver2@gmail.com>
2025-01-09 10:15:49 -0600

    Add leetcode-problem-hint.component.ts to be an Angular component for the leetcode-problem-hint element in the Angular application

---
+++ angular/components/elements/leetcode-problem-hint/leetcode-problem-hint.component.ts
@@ -0,0 +1,14 @@
+// leetcode-problem-hint.component.ts 
+import {Component, Input} from "@angular/core";
+
+@Component({
+    selector: 'leetcode-problem-hint',
+    templateUrl: './leetcode-problem-hint.component.html',
+    styleUrls: ['./leetcode-problem-hint-style.component.css']
+})
+export class LeetCodeProblemHintComponent {
+    @Input() hint: string;
+    constructor() {
+        
+    }
+}
