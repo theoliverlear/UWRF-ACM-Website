0d887a1963b56b99cdf364502771a053788c2dd1
theoliverlear <sigwartholiver2@gmail.com>
2025-01-14 19:05:21 -0600

    Add DiscordTextHighlightDirective to directives.ts to add text highlighting of the word "Discord" functionality

--- angular/directives/directives.ts
+++ angular/directives/directives.ts
@@ -1,8 +1,12 @@
 import {UndraggableDirective} from "./undraggable.directive";
 import {UnoptimizedImageDirective} from "./unoptimized-image.directive";
 import {RumbleDirective} from "./rumble.directive";
+import {
+    DiscordTextHighlightDirective
+} from "./discord-text-highlight.directive";
 
 export const directives = [
+    DiscordTextHighlightDirective,
     RumbleDirective,
     UndraggableDirective,
     UnoptimizedImageDirective
