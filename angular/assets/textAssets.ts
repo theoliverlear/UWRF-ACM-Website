c4e068716ad2f5cb77687b009efa6e5eacdeb758
theoliverlear <sigwartholiver2@gmail.com>
2025-01-15 09:58:20 -0600

    Update textAssets.ts with additional promotional text prompts

--- angular/assets/textAssets.ts
+++ angular/assets/textAssets.ts
@@ -1,3 +1,8 @@
-export const promoTextFullTitle: string = "We're the Association of Computing Machinery.";
+export const promoTextFullTitle: string = "We're the Association for Computing Machinery.";
 export const promoTextCallUs: string = "You can call us..."
 export const promoTextAcronym: string = "ACM"
+export const promoTextWhatAcmDoesPrompt: string = "What does ACM do?";
+export const promoTextMeetProfessions: string = "Meet professionals";
+export const promoTextSoftwareDevelopment: string = "Learn software development";
+export const promoTextBuildTech: string = "Build apps, games, websites";
+export const promoTextFormFriendships: string = "Form lifelong friendships";
