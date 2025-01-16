768832002a01d7a6a5a46b073de65ee180c64dd4
theoliverlear <sigwartholiver2@gmail.com>
2025-01-16 07:17:59 -0600

    Add metaAssets.ts to define meta tag content in the Angular application

---
+++ angular/assets/metaAssets.ts
@@ -0,0 +1,13 @@
+import {MetaDefinition} from "@angular/platform-browser";
+
+export const descriptionMeta: MetaDefinition = {
+    name: 'description',
+    content: 'The University of Wisconsin - River Falls chapter of the ' +
+        'ACM is the place to be for tech enthusiasts, budding programmers,' +
+        ' and anyone interested in the world of computing.'
+};
+export const keywordsMeta: MetaDefinition = {
+    name: 'keywords',
+    content: 'UWRF, ACM, Association for Computing Machinery, ' +
+        'Computer Science, Technology, Programming, Software Development'
+};
