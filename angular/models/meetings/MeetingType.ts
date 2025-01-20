7ac3e30c060af160a5e9df73c09cbf92c2dd1f4c
theoliverlear <sigwartholiver2@gmail.com>
2025-01-20 12:38:03 -0600

    Add MeetingType.ts to define different meeting types as an enum in the Angular application

---
+++ angular/models/meetings/MeetingType.ts
@@ -0,0 +1,8 @@
+export enum MeetingType {
+    SEMINAR = 'Seminar',
+    WORKSHOP = 'Workshop',
+    CONFERENCE = 'Conference',
+    LEETCODE = 'LeetCode',
+    GUEST_SPEAKER = 'Guest Speaker',
+    STUDY_SESSION = 'Study Session',
+}
