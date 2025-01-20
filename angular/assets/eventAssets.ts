a70d8193368c7a2ce670849ffe9b828ea4f30e08
theoliverlear <sigwartholiver2@gmail.com>
2025-01-20 17:09:34 -0600

    Edit event titles for consistent capitalization in eventAssets.ts

--- angular/assets/eventAssets.ts
+++ angular/assets/eventAssets.ts
@@ -6,10 +6,10 @@ const defaultAcmMeetingPlace = 'South Hall 21 and Virtually In Discord';
 export const firstMeetingOfSemester: MeetingEvent = new MeetingEvent('First Meeting of Semester',
                                                                      getMeetingDateTime('2025-02-06'),
                                                                      defaultAcmMeetingPlace);
-export const readabilityMeeting: MeetingEvent = new MeetingEvent("Code quality and readability seminar",
+export const readabilityMeeting: MeetingEvent = new MeetingEvent("Code Quality and Readability Seminar",
                                                                  getMeetingDateTime('2025-02-13'),
                                                                 defaultAcmMeetingPlace);
-export const shaneMeeting: MeetingEvent = new MeetingEvent('Shane O\'Malley Potting talks to ACM',
+export const shaneMeeting: MeetingEvent = new MeetingEvent('Shane O\'Malley Potting Talks to ACM',
                                                                     getMeetingDateTime('2025-02-20'),
                                                                     defaultAcmMeetingPlace);
 export const lastMeetingOfSemester: MeetingEvent = new MeetingEvent('Last Meeting of Semester',
