e8b3c19fb57dc261dbcf5673a4fd6c3ab7e241d0
theoliverlear <sigwartholiver2@gmail.com>
2025-01-20 12:53:22 -0600

    Add guestSpeakerAssets.ts to define GuestSpeaker assets for meetings in the Angular application

---
+++ angular/assets/guestSpeakerAssets.ts
@@ -0,0 +1,5 @@
+import {GuestSpeaker} from "../models/meetings/GuestSpeaker";
+
+export let shaneOMalleyPotting: GuestSpeaker;
+export let samChapin: GuestSpeaker;
+export let ethanPrice: GuestSpeaker;
