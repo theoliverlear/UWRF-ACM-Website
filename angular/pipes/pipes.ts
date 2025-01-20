2aeac20a9f252fb9d7ecb26c4e87c8e896ff5db3
theoliverlear <sigwartholiver2@gmail.com>
2025-01-20 15:29:21 -0600

    Add CommaListPipe to pipes array in pipes.ts to inject new pipe functionality in the Angular application

--- angular/pipes/pipes.ts
+++ angular/pipes/pipes.ts
@@ -1,5 +1,7 @@
 import {DateDifferencePipe} from "./date-difference.pipe";
+import {CommaListPipe} from "./comma-list.pipe";
 
 export const pipes: any[] = [
+    CommaListPipe,
     DateDifferencePipe
 ];
