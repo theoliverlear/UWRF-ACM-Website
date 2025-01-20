1e7487a49c1c6bc3218aa9c304b3ed24b92e9570
theoliverlear <sigwartholiver2@gmail.com>
2025-01-20 17:09:59 -0600

    Add Shane O'Malley-Potting guest speaker details and bio in guestSpeakerAssets.ts

--- angular/assets/guestSpeakerAssets.ts
+++ angular/assets/guestSpeakerAssets.ts
@@ -1,5 +1,22 @@
 import {GuestSpeaker} from "../models/meetings/GuestSpeaker";
+import {shaneOMalleyPottingImageAsset} from "./imageAssets";
 
-export let shaneOMalleyPotting: GuestSpeaker;
-export let samChapin: GuestSpeaker;
-export let ethanPrice: GuestSpeaker;
+const shaneOMalleyPottingBio: string = "Shane O'Malley-Potting has come to share his inspirational journey from " +
+    "student worker at UWRF to the backbone of UWRF's infrastructure. He has" +
+    " come to share his story and wisdom in computer science and life.";
+const shaneOMalleyPottingTopics: string[] = [
+    "Career Advice",
+    "Infrastructure",
+    "Student Success",
+    "Migrating Legacy Systems"
+];
+export let shaneOMalleyPottingGuestSpeaker: GuestSpeaker = new GuestSpeaker(
+    "Shane",
+    "O'Malley-Potting",
+    shaneOMalleyPottingTopics,
+    shaneOMalleyPottingBio,
+    shaneOMalleyPottingImageAsset,
+    "Information Technology Architect at UW-River Falls"
+)
+export let samChapinGuestSpeaker: GuestSpeaker;
+export let ethanPriceGuestSpeaker: GuestSpeaker;
