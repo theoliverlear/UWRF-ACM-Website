74c81fad2bf25f09e73db6472f31102afaa474b3
theoliverlear <sigwartholiver2@gmail.com>
2025-01-20 13:02:11 -0600

    Add trivia-category.component.ts to be an Angular component for the trivia-category element in the Angular application

---
+++ angular/components/elements/trivia-category/trivia-category.component.ts
@@ -0,0 +1,13 @@
+// trivia-category.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'trivia-category',
+    templateUrl: './trivia-category.component.html',
+    styleUrls: ['./trivia-category-style.component.css']
+})
+export class TriviaCategoryComponent {
+    constructor() {
+        
+    }
+}
