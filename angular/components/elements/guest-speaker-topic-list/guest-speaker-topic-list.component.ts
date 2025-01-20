23d5fbd32631b6b84e47668e88e7e0a13ad36f3f
theoliverlear <sigwartholiver2@gmail.com>
2025-01-20 17:25:04 -0600

    Add guest-speaker-topic-list.component.ts to be an Angular component for the guest-speaker-topic-list element in the Angular application

---
+++ angular/components/elements/guest-speaker-topic-list/guest-speaker-topic-list.component.ts
@@ -0,0 +1,17 @@
+// guest-speaker-topic-list.component.ts 
+import {Component, Input} from "@angular/core";
+import {TagType} from "../../../models/html/TagType";
+
+@Component({
+    selector: 'guest-speaker-topic-list',
+    templateUrl: './guest-speaker-topic-list.component.html',
+    styleUrls: ['./guest-speaker-topic-list-style.component.css']
+})
+export class GuestSpeakerTopicListComponent {
+    @Input() topics: string[];
+    constructor() {
+        
+    }
+
+    protected readonly TagType = TagType;
+}
