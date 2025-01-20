b9a454fe2a4c9ca6d995749765a681ce7e14fbbc
theoliverlear <sigwartholiver2@gmail.com>
2025-01-20 17:25:55 -0600

    Add subtitle.component.ts to be an Angular component for the subtitle element in the Angular application

---
+++ angular/components/elements/subtitle/subtitle.component.ts
@@ -0,0 +1,18 @@
+// subtitle.component.ts 
+import {Component, Input} from "@angular/core";
+import {TagType} from "../../../models/html/TagType";
+
+@Component({
+    selector: 'subtitle',
+    templateUrl: './subtitle.component.html',
+    styleUrls: ['./subtitle-style.component.css']
+})
+export class SubtitleComponent {
+    @Input() tagType: TagType = TagType.H2;
+    @Input() text: string;
+    constructor() {
+        
+    }
+
+    protected readonly TagType = TagType;
+}
