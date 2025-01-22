aaccaae73541fea19a19360c0de5f96d241e1e93
theoliverlear <sigwartholiver2@gmail.com>
2025-01-22 12:03:41 -0600

    Add input property and TagType reference in trivia.component.ts

--- angular/components/elements/trivia/trivia.component.ts
+++ angular/components/elements/trivia/trivia.component.ts
@@ -1,5 +1,7 @@
-// trivia.component.ts 
-import { Component } from "@angular/core";
+// trivia.component.ts
+import {Component, Input} from "@angular/core";
+import {TriviaGame} from "../../../models/meetings/trivia/TriviaGame";
+import {TagType} from "../../../models/html/TagType";
 
 @Component({
     selector: 'trivia',
@@ -7,7 +9,9 @@ import { Component } from "@angular/core";
     styleUrls: ['./trivia-style.component.css']
 })
 export class TriviaComponent {
+    @Input() protected triviaGame: TriviaGame;
     constructor() {
         
     }
+    protected readonly TagType = TagType;
 }
