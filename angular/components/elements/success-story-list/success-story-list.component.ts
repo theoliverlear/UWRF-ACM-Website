2d85c4d56849adae8987cc436457e1b0cca7b897
theoliverlear <sigwartholiver2@gmail.com>
2025-01-22 17:20:28 -0600

    Add success-story-list.component.ts to be an Angular component for the success-story-list element in the Angular application

---
+++ angular/components/elements/success-story-list/success-story-list.component.ts
@@ -0,0 +1,15 @@
+// success-story-list.component.ts 
+import {Component, Input} from "@angular/core";
+import {SuccessStory} from "../success-story/models/SuccessStory";
+
+@Component({
+    selector: 'success-story-list',
+    templateUrl: './success-story-list.component.html',
+    styleUrls: ['./success-story-list-style.component.css']
+})
+export class SuccessStoryListComponent {
+    @Input() protected successStories: SuccessStory[];
+    constructor() {
+        
+    }
+}
