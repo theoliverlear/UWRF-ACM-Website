7c3e7f3f131f4620710a9060773124bd7f75d3df
theoliverlear <sigwartholiver2@gmail.com>
2025-01-22 17:00:21 -0600

    Add successStoryAssets.ts to include success story content used throughout the Angular application

---
+++ angular/assets/successStoryAssets.ts
@@ -0,0 +1,17 @@
+import {
+    SuccessStoryMessage
+} from "../components/elements/success-story/models/SuccessStoryMessage";
+import {randyFranzmeierImageAsset} from "./imageAssets";
+
+export const randyFranzmeierSuccessStory = new SuccessStoryMessage(
+    'Randy Franzmeier',
+    'Junior Software Engineer',
+    'Tanduo Technical Patterns Inc.',
+    'In Dev Team, Randy worked on the full-stack development of our websites. ' +
+    'He learned about various technologies and tools like Angular and SCSS' +
+    ' to bring success to our projects.',
+    'Dev Team helped me become a better developer and team player. I learned a lot ' +
+    'about how to work with a team and how to develop software. I am ' +
+    'grateful for the opportunity to work with such a great team.',
+    randyFranzmeierImageAsset
+);
