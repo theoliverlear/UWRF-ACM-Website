cb193c09b447e6d75239ec03f9656ffbae73dab1
theoliverlear <sigwartholiver2@gmail.com>
2025-01-22 18:48:39 -0600

    Add multiple TextElementLink instances for various project source code links in elementLinkAssets.ts

--- angular/assets/elementLinkAssets.ts
+++ angular/assets/elementLinkAssets.ts
@@ -26,4 +26,40 @@ export const navBarDevTeamElementLink = new TextElementLink('/dev-team',
                                                             'Dev Team',
                                                             TagType.H4);
 export const discordElementLink = new ElementLink('https://discord.gg/EJUJEBKgHJ',
-                                                      TargetType.BLANK)
+                                                  TargetType.BLANK)
+
+export const currencyConverterElementLink = new TextElementLink('https://github.com/UWRF-Development-Team/Currency-Converter',
+                                                                TargetType.BLANK,
+                                                                false,
+                                                                'View Source Code',
+                                                                TagType.H6);
+export const gamesWebsiteElementLink = new TextElementLink('https://github.com/UWRF-Development-Team/Games-Website/tree/SpringV3',
+                                                           TargetType.BLANK,
+                                                           false,
+                                                           'View Source Code',
+                                                           TagType.H6);
+export const dadJokeApiElementLink = new TextElementLink('https://github.com/UWRF-Development-Team/Dad-Joke-API/tree/SpringAPI',
+                                                         TargetType.BLANK,
+                                                         false,
+                                                         'View Source Code',
+                                                         TagType.H6);
+export const devTeamWebsiteElementLink = new TextElementLink('https://github.com/UWRF-Development-Team/Dev-Team-Website',
+                                                             TargetType.BLANK,
+                                                             false,
+                                                             'View Source Code',
+                                                             TagType.H6);
+export const chessElementLink = new TextElementLink('https://github.com/UWRF-Development-Team/Chess/tree/main',
+                                                    TargetType.BLANK,
+                                                    false,
+                                                    'View Source Code',
+                                                    TagType.H6);
+export const taskManagerElementLink = new TextElementLink('https://github.com/UWRF-Development-Team/Task-Manager',
+                                                          TargetType.BLANK,
+                                                          false,
+                                                          'View Source Code',
+                                                          TagType.H6);
+export const geoflagElementLink = new TextElementLink('https://github.com/UWRF-Development-Team/GeoFlag',
+                                                      TargetType.BLANK,
+                                                      false,
+                                                      'View Source Code',
+                                                      TagType.H6);
