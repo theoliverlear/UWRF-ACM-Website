d7448ac65163df746d038e83ab12b3579fb9e449
theoliverlear <sigwartholiver2@gmail.com>
2025-01-22 18:54:06 -0600

    Add dev-team-project.component.ts to be an Angular component for the dev-team-project element in the Angular application

---
+++ angular/components/elements/dev-team-project/dev-team-project.component.ts
@@ -0,0 +1,18 @@
+// dev-team-project.component.ts 
+import {Component, Input} from "@angular/core";
+import {DevTeamProject} from "./models/DevTeamProject";
+import {TagType} from "../../../models/html/TagType";
+
+@Component({
+    selector: 'dev-team-project',
+    templateUrl: './dev-team-project.component.html',
+    styleUrls: ['./dev-team-project-style.component.css']
+})
+export class DevTeamProjectComponent {
+    @Input() protected project: DevTeamProject;
+    constructor() {
+        
+    }
+
+    protected readonly TagType = TagType;
+}
