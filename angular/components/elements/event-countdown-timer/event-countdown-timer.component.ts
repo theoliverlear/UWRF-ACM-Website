eccc2a78c795628944d49468c3960b2580b087ea
theoliverlear <sigwartholiver2@gmail.com>
2025-01-24 11:28:44 -0600

    Refactor event-countdown-timer.component.ts to use Luxon for date-time operations, extract methods and variables, and add access modifiers

--- angular/components/elements/event-countdown-timer/event-countdown-timer.component.ts
+++ angular/components/elements/event-countdown-timer/event-countdown-timer.component.ts
@@ -1,6 +1,7 @@
 // event-countdown-timer.component.ts 
 import {ChangeDetectorRef, Component, Input, OnInit} from "@angular/core";
 import {DateInterval} from "./models/DateInterval";
+import {DateTime} from "luxon";
 
 @Component({
     selector: 'event-countdown-timer',
@@ -8,32 +9,34 @@ import {DateInterval} from "./models/DateInterval";
     styleUrls: ['./event-countdown-timer-style.component.css']
 })
 export class EventCountdownTimerComponent implements OnInit {
-    @Input() eventDate: Date;
-    readonly meetingDurationInMinutes: number = 120;
-    isDuringEvent: boolean = false;
-    remainingTime: number;
+    @Input() protected eventDate: DateTime;
+    protected readonly meetingDurationInMinutes: number = 120;
+    protected isDuringEvent: boolean = false;
+    protected remainingTime: number;
     constructor(private changeDetector: ChangeDetectorRef) {
         
     }
-    ngOnInit() {
+    ngOnInit(): void {
         this.updateTime();
-        setInterval(() => this.updateTime(), 1000);
+        this.continuouslyUpdateTime();
     }
-    updateTime() {
-        const currentTime: number = new Date().getTime();
-        const eventStartTime: number = this.eventDate.getTime();
+    private continuouslyUpdateTime(): void {
+        setInterval((): void => this.updateTime(), 1000);
+    }
+    private updateTime(): void {
+        const currentTime: number = DateTime.now().setZone('America/Chicago').toMillis();
+        const eventStartTime: number = this.eventDate.toMillis();
         const eventEndTime: number = eventStartTime + this.meetingDurationInMinutes * 60 * 1000;
-        this.remainingTime = (eventEndTime - currentTime) / 1000;
+        this.remainingTime = (eventStartTime - currentTime) / 1000;
         this.setEventIsNow(currentTime, eventStartTime, eventEndTime);
         this.changeDetector.detectChanges();
     }
-    setEventIsNow(currentTime: number,
+    private setEventIsNow(currentTime: number,
                   eventStartTime: number,
                   eventEndTime: number): void {
-        this.isDuringEvent = currentTime >= eventStartTime && currentTime <= eventEndTime;
-    }
-    isNow() {
-        return this.isDuringEvent;
+        const hasEventStarted: boolean = currentTime >= eventStartTime;
+        const hasEventEnded: boolean = eventEndTime <= currentTime;
+        this.isDuringEvent = hasEventStarted && !hasEventEnded;
     }
     protected readonly DateInterval = DateInterval;
 }
