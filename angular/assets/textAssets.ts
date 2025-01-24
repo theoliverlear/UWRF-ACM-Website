993df6dc253060e9695b84af5470dc689f5008d1
theoliverlear <sigwartholiver2@gmail.com>
2025-01-24 11:35:26 -0600

    Add text assets for Dev Team, its activities, and participation modes in textAssets.ts

--- angular/assets/textAssets.ts
+++ angular/assets/textAssets.ts
@@ -6,3 +6,56 @@ export const promoTextMeetProfessions: string = "Meet professionals";
 export const promoTextSoftwareDevelopment: string = "Learn software development";
 export const promoTextBuildTech: string = "Build apps, games, websites";
 export const promoTextFormFriendships: string = "Form lifelong friendships";
+
+export const devTeamSummary: string = "The UWRF Development Team, or Dev " +
+    "Team, is a group inside of ACM working on programming projects. Even" +
+    " for entry-level positions, development team experience is sought " +
+    "after. Dev Team offers the ability to get real-world development team" +
+    " experience while in college. Dev Team works on many projects " +
+    "collaboratively spanning from games to mobile apps to websites. We at" +
+    " Dev Team welcome programmers at all levels. We are a welcoming group" +
+    " that is here to learn and grow with the projects we develop. Many " +
+    "people have found success in the industry citing Dev Team as a factor" +
+    " in their programming and teamwork ability.";
+export const devTeamMeetingInfo: string = "Tuesdays, 4:30-6:30 PM, in South" +
+                                          " Hall 21 and virtually in Discord";
+export const devTeamVersatile: string = "As a club, Dev Team is in a unique" +
+    " position to offer a versatile mode of participation in the club. " +
+    "There are many ways our members choose to participate. All of the " +
+    "following ways are available to anyone, and we welcome you to choose " +
+    "one that fits your needs."
+export const devTeamAsynchronous: string = "Using GitHub repositories, our " +
+    "members can contribute anytime from anywhere. You can write some code" +
+    " on your computer and your work will be seen by the rest of the group." +
+    " We also utilize JetBrains’ “Code With Me” feature. This feature allows" +
+    " users to edit code together like a Google Doc. This is a favored way " +
+    "Dev Team operates in conjunction with our Discord messaging and voice " +
+    "functionality. Our asynchronous ability is best for those with busy " +
+    "schedules who may want to contribute, but can’t always make it to the" +
+    " meeting time.";
+export const devTeamVirtually: string = "Through our community Discord," +
+    " anyone can join a meeting from anywhere. This is convenient for those" +
+    " who do not live on campus or just prefer to stay home. You can do" +
+    " almost everything our other members can do by utilizing our GitHub" +
+    " and Code With Me options. The group has telecommunication capability" +
+    " through Discord, so your presence will be just as if you were in the" +
+    " room.";
+export const devTeamInPerson: string = "Our in-person option, in South Hall" +
+    " 21, is a popular choice for those who enjoy the resources of the " +
+    "room. The room has computers, microcomputers, servers, switches, and" +
+    " robotics materials. These resources are best accessed in person where" +
+    " you can tinker with it. Meeting in person is a great way to meet and" +
+    " chat with new friends you may make along the way. We often have " +
+    "celebrations at Dev Team where those in-person get access to food," +
+    " drinks, candy, and prizes offered at such meetings.";
+export const devTeamFinalMessage: string = "This group has all the " +
+    "resources of ACM at its disposal to welcome all majors from Computer" +
+    " Science to Data Science to Information Systems. We have a computer " +
+    "lab for those without a computer and we can connect you with free" +
+    " resources to jumpstart you’re programming with IntelliJ IDEA Ultimate" +
+    " and GitHub Copilot. We also have various computers, microcomputers," +
+    " Raspberry Pi’s, and servers that anyone can tamper with and build " +
+    "projects on. Dev Team isn’t just a place to program, but a place to " +
+    "build your resume. You can show employers your real-world work in Dev " +
+    "Team. We at Dev Team are proud of our ability to create projects our " +
+    "members want and appeal to what employers need.";
