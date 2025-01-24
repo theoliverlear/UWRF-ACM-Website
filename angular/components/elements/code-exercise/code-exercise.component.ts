cdc5e6a500f800de7aa3fa6a384d97eaf70deb90
theoliverlear <sigwartholiver2@gmail.com>
2025-01-24 16:49:48 -0600

    Add code-exercise.component.ts to be an Angular component for the code-exercise element in the Angular application

---
+++ angular/components/elements/code-exercise/code-exercise.component.ts
@@ -0,0 +1,18 @@
+// code-exercise.component.ts 
+import {Component, Input} from "@angular/core";
+import {EditableCode} from "../../../models/meetings/code/EditableCode";
+
+@Component({
+    selector: 'code-exercise',
+    templateUrl: './code-exercise.component.html',
+    styleUrls: ['./code-exercise-style.component.css']
+})
+export class CodeExerciseComponent {
+    @Input() protected codeExercises: EditableCode[];
+    constructor() {
+        
+    }
+    protected getPossibleTitle(editableCode: EditableCode): string | undefined {
+        return editableCode.title ? editableCode.title : undefined;
+    }
+}
