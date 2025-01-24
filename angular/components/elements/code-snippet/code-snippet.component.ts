1bcc9914f7622a5cff3e9185952d9773ce891111
theoliverlear <sigwartholiver2@gmail.com>
2025-01-24 16:50:57 -0600

    Add code-snippet.component.ts to be an Angular component for the code-snippet element in the Angular application

---
+++ angular/components/elements/code-snippet/code-snippet.component.ts
@@ -0,0 +1,13 @@
+// code-snippet.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'code-snippet',
+    templateUrl: './code-snippet.component.html',
+    styleUrls: ['./code-snippet-style.component.css']
+})
+export class CodeSnippetComponent {
+    constructor() {
+        
+    }
+}
