3720cf70bd7a35e6896d1977535be089b0570ab8
theoliverlear <sigwartholiver2@gmail.com>
2025-01-24 11:41:11 -0600

    Add MILLISECONDS_IN_WEEK constant in TimeConversion.ts

--- angular/models/TimeConversion.ts
+++ angular/models/TimeConversion.ts
@@ -8,4 +8,9 @@ export enum TimeConversion {
     // 2+ levels of precision
     SECONDS_IN_HOUR = TimeConversion.SECONDS_IN_MINUTE * TimeConversion.MINUTES_IN_HOUR,
     SECONDS_IN_DAY = TimeConversion.SECONDS_IN_HOUR * TimeConversion.HOURS_IN_DAY,
+    MILLISECONDS_IN_WEEK = TimeConversion.DAYS_IN_WEEK *
+                           TimeConversion.HOURS_IN_DAY *
+                           TimeConversion.MINUTES_IN_HOUR *
+                           TimeConversion.SECONDS_IN_MINUTE *
+                           TimeConversion.MILLIS_IN_SECOND,
 }
