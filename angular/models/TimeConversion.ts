517eb4333add7c2ab01ddfc323e9916c14fa09ec
theoliverlear <sigwartholiver2@gmail.com>
2025-01-24 11:05:49 -0600

    Add constants for SECONDS_IN_HOUR and SECONDS_IN_DAY with multi-level precision in TimeConversion.ts

--- angular/models/TimeConversion.ts
+++ angular/models/TimeConversion.ts
@@ -1,7 +1,11 @@
 export enum TimeConversion {
+    // 1 level of precision
     MILLIS_IN_SECOND = 1000,
     SECONDS_IN_MINUTE = 60,
     MINUTES_IN_HOUR = 60,
     HOURS_IN_DAY = 24,
     DAYS_IN_WEEK = 7,
+    // 2+ levels of precision
+    SECONDS_IN_HOUR = TimeConversion.SECONDS_IN_MINUTE * TimeConversion.MINUTES_IN_HOUR,
+    SECONDS_IN_DAY = TimeConversion.SECONDS_IN_HOUR * TimeConversion.HOURS_IN_DAY,
 }
