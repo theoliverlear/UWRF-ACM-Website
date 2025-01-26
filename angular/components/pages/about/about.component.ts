78699b026aa3be57408845f8983bb09b13dd712c
theoliverlear <sigwartholiver2@gmail.com>
2025-01-26 13:57:06 -0600

    Add animations, assets, and lifecycle method to AboutComponent in about.component.ts

--- angular/components/pages/about/about.component.ts
+++ angular/components/pages/about/about.component.ts
@@ -1,13 +1,49 @@
 // about.component.ts 
-import { Component } from "@angular/core";
+import {AfterViewInit, Component, ViewChild} from "@angular/core";
+import {
+    acmMemberImageAssets,
+    logoImageAsset
+} from "../../../assets/imageAssets";
+import {TagType} from "../../../models/html/TagType";
+import {
+    acmFinalMessage,
+    acmMeetingInfo,
+    acmSummary,
+    whatAcmDoesAnswers
+} from "../../../assets/textAssets";
+import {DelayService} from "../../../services/delay.service";
+import {
+    AcmActionListComponent
+} from "../../elements/acm-action-list/acm-action-list.component";
+import {slideLeftAnimation} from "../../animations/animations";
+import {
+    nineSecondDelayMediumFadeInAnimationProperties
+} from "../../animations/animationProperties";
 
 @Component({
     selector: 'about',
     templateUrl: './about.component.html',
-    styleUrls: ['./about-style.component.css']
+    styleUrls: ['./about-style.component.css'],
+    animations: [
+        slideLeftAnimation
+    ]
 })
-export class AboutComponent {
-    constructor() {
+export class AboutComponent implements AfterViewInit {
+    @ViewChild(AcmActionListComponent) protected acmActionListComponent: AcmActionListComponent;
+    constructor(private delayService: DelayService) {
         
     }
+    public async ngAfterViewInit(): Promise<void> {
+        await this.delayService.delay(1000);
+        console.log('sliding in');
+        await this.acmActionListComponent.cascadeSlideIn();
+    }
+    protected readonly logoImageAsset = logoImageAsset;
+    protected readonly TagType = TagType;
+    protected readonly acmMeetingInfo = acmMeetingInfo;
+    protected readonly acmSummary = acmSummary;
+    protected readonly whatAcmDoesAnswers = whatAcmDoesAnswers;
+    protected readonly acmFinalMessage = acmFinalMessage;
+    protected readonly nineSecondDelayMediumFadeInAnimationProperties = nineSecondDelayMediumFadeInAnimationProperties;
+    protected readonly acmMemberImageAssets = acmMemberImageAssets;
 }
