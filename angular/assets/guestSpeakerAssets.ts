629ded5b685465593c1bdb709461edd116678664
theoliverlear <sigwartholiver2@gmail.com>
2025-02-08 16:27:41 -0600

    Add bios, topics, and guest speaker objects for Ethan Price and Sam Chapin in guestSpeakerAssets.ts

--- angular/assets/guestSpeakerAssets.ts
+++ angular/assets/guestSpeakerAssets.ts
@@ -1,5 +1,8 @@
 import {GuestSpeaker} from "../models/meetings/GuestSpeaker";
-import {shaneOMalleyPottingImageAsset} from "./imageAssets";
+import {
+    ethanPriceImageAsset, samChapinImageAsset,
+    shaneOMalleyPottingImageAsset
+} from "./imageAssets";
 
 const shaneOMalleyPottingBio: string = "Shane O'Malley-Potting has come to share his inspirational journey from " +
     "student worker at UWRF to the backbone of UWRF's infrastructure. He has" +
@@ -18,5 +21,39 @@ export let shaneOMalleyPottingGuestSpeaker: GuestSpeaker = new GuestSpeaker(
     shaneOMalleyPottingImageAsset,
     "Information Technology Architect at UW-River Falls"
 )
-export let samChapinGuestSpeaker: GuestSpeaker;
-export let ethanPriceGuestSpeaker: GuestSpeaker;
+export const ethanPriceBio: string = "Ethan Price is a former ACM officer and a current employee at Epic Healthcare Systems. " +
+    "He has come to talk about resumes, interviews, and technical knowledge. Being part of these rigorous procedures himself, " +
+    "Ethan knows the do's and don'ts in job applications and interviews. ACM will also serve our signature Dominos pizza and drinks. " +
+    "Come join us for knowledge, fun, and tasty pizza!";
+export const ethanPriceTopics: string[] = [
+    "Resumes",
+    "Interviews",
+    "Technical Knowledge",
+    "Job Applications"
+];
+export let ethanPriceGuestSpeaker: GuestSpeaker = new GuestSpeaker(
+    "Ethan",
+    "Price",
+    ethanPriceTopics,
+    ethanPriceBio,
+    ethanPriceImageAsset,
+    "Employee at Epic Healthcare Systems"
+)
+export const samChapinBio: string = "Sam Chapin is a community contributor as an alumni and the donator of the ACM Chapin Server. " +
+    "He works at the prestigious Thomson Reuters as a Senior Software Engineer. He has come to speak about the computer science industry. " +
+    "He will provide his wide-ranging knowledge in a Q and A, as well as talk to us about vitally important topics like: intern and entry-level expectations, " +
+    "modern software development methodology, and the responsibilities expected of different roles in the industry.";
+export const samChapinTopics: string[] = [
+    "Job Expectations",
+    "Methodology",
+    "Role Duties",
+    "CI/CD"
+];
+export let samChapinGuestSpeaker: GuestSpeaker = new GuestSpeaker(
+    "Sam",
+    "Chapin",
+    samChapinTopics,
+    samChapinBio,
+    samChapinImageAsset,
+    "Senior Software Engineer at Thomson Reuters"
+)
