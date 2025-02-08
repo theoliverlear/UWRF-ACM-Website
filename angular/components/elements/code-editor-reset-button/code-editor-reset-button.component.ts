c0f0b828a9152c79ff0ad931ff39b7d1db4b83c3
theoliverlear <sigwartholiver2@gmail.com>
2025-02-08 16:47:10 -0600

    Add code-editor-reset-button.component.ts to be an Angular component for the code-editor-reset-button element in the Angular application

---
+++ angular/components/elements/code-editor-reset-button/code-editor-reset-button.component.ts
@@ -0,0 +1,13 @@
+// code-editor-reset-button.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'code-editor-reset-button',
+    templateUrl: './code-editor-reset-button.component.html',
+    styleUrls: ['./code-editor-reset-button-style.component.css']
+})
+export class CodeEditorResetButtonComponent {
+    constructor() {
+        
+    }
+}
