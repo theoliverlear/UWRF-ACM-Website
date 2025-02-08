f2f50d74d3c767809db1145f72ce9c203d844c77
theoliverlear <sigwartholiver2@gmail.com>
2025-02-08 16:28:51 -0600

    Add Colton Williams success story and image asset in successStoryAssets.ts

--- angular/assets/successStoryAssets.ts
+++ angular/assets/successStoryAssets.ts
@@ -1,7 +1,10 @@
 import {
     SuccessStoryMessage
 } from "../components/elements/success-story/models/SuccessStoryMessage";
-import {randyFranzmeierImageAsset} from "./imageAssets";
+import {
+    coltonWilliamsImageAsset,
+    randyFranzmeierImageAsset
+} from "./imageAssets";
 import {
     SuccessStory
 } from "../components/elements/success-story/models/SuccessStory";
@@ -19,6 +22,23 @@ export const randyFranzmeierSuccessStory = new SuccessStoryMessage(
     'internship and a full time job as a software engineer!',
     randyFranzmeierImageAsset
 );
+export const coltonWilliamsSuccessStory = new SuccessStoryMessage(
+    'Colton Williams',
+    'Support Engineer',
+    'SkyTide Group',
+    'As an information systems leader, Colton helped Dev Team build its ' +
+    'infrastructure. He helped set up various servers and desktops with the' +
+    ' necessary content needed to allow Dev Team to flourish. He is also' +
+    ' noted for his ability to research and quickly find useful information ' +
+    'to guide the team.',
+    'The UWRF Development Team provided me with invaluable hands-on ' +
+    'experience that helped me grow both technically and professionally. ' +
+    'The skills I gained while working on mock real-world projects were' +
+    ' crucial in boosting my confidence and capabilities, allowing me to' +
+    ' confidently pursue job opportunities after graduation.',
+    coltonWilliamsImageAsset
+);
 export const successStories: SuccessStory[] = [
-    randyFranzmeierSuccessStory
+    randyFranzmeierSuccessStory,
+    coltonWilliamsSuccessStory
 ];
