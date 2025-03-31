42d8801bdb693445b2b01e34c29a14e7203b1f50
theoliverlear <sigwartholiver2@gmail.com>
2025-03-31 17:37:54 -0500

    Add new LeetCode problem definitions to leetCodeAssets.ts for Fibonacci, sorting, and binary tree traversal

--- angular/assets/leetCodeAssets.ts
+++ angular/assets/leetCodeAssets.ts
@@ -9,3 +9,33 @@ export const fizzBuzzLeetCodeProblem: LeetCodeProblem = new LeetCodeProblem(
 export const weekOneLeetCodeProblems: LeetCodeProblem[] = [
     fizzBuzzLeetCodeProblem
 ];
+
+export const fibonacciLeetCodeProblem: LeetCodeProblem = new LeetCodeProblem(
+    'Fibonacci Number',
+    ProblemDifficulty.EASY,
+    'The Fibonacci numbers, commonly denoted F(n) form a sequence, called' +
+    ' the Fibonacci sequence, such that each number is the sum of the two ' +
+    'preceding ones.',
+    'https://leetcode.com/problems/fibonacci-number/',
+    'This problem is based on recursion of the two values previous.'
+);
+export const sortAnArrayLeetCodeProblem: LeetCodeProblem = new LeetCodeProblem(
+    'Sort an Array',
+    ProblemDifficulty.MEDIUM,
+    'Given an array of integers nums, sort the array in ascending order.',
+    'https://leetcode.com/problems/sort-an-array/',
+    'Look into efficient algorithms like Quick Sort and Merge Sort.'
+);
+export const binaryTreeInorderTraversalLeetCodeProblem: LeetCodeProblem = new LeetCodeProblem(
+    'Binary Tree Traversal',
+    ProblemDifficulty.EASY,
+    'Given the root of a binary tree, return the inorder traversal of its nodes\' values.',
+    'https://leetcode.com/problems/binary-tree-inorder-traversal/',
+    'Tackle the left side, move up, then tackle the right side.'
+);
+
+export const micsPreparationLeetCodeProblems: LeetCodeProblem[] = [
+    fibonacciLeetCodeProblem,
+    sortAnArrayLeetCodeProblem,
+    binaryTreeInorderTraversalLeetCodeProblem
+];
