43e4b27a8ba3012c58369ca01571540662580e01
theoliverlear <sigwartholiver2@gmail.com>
2025-03-31 17:37:25 -0500

    Add Replit element link for the debugging exercise in elementLinkAssets.ts

--- angular/assets/elementLinkAssets.ts
+++ angular/assets/elementLinkAssets.ts
@@ -4,62 +4,69 @@ import {ElementLink} from "../models/link/ElementLink";
 import {TagType} from "../models/html/TagType";
 
 export const navBarHomeElementLink = new ElementLink('/',
-                                                     TargetType.SELF);
+    TargetType.SELF);
 export const navBarJoinElementLink = new TextElementLink('/join',
-                                                         TargetType.SELF,
-                                                         false,
-                                                         'Join',
-                                                         TagType.H4);
+    TargetType.SELF,
+    false,
+    'Join',
+    TagType.H4);
 export const navBarAboutElementLink = new TextElementLink('/about',
-                                                          TargetType.SELF,
-                                                          false,
-                                                          'About',
-                                                          TagType.H4);
+    TargetType.SELF,
+    false,
+    'About',
+    TagType.H4);
 export const navBarThisWeekElementLink = new TextElementLink('/this-week',
-                                                             TargetType.SELF,
-                                                             false,
-                                                             'This Week',
-                                                             TagType.H4);
+    TargetType.SELF,
+    false,
+    'This Week',
+    TagType.H4);
 export const navBarDevTeamElementLink = new TextElementLink('/dev-team',
-                                                            TargetType.SELF,
-                                                            false,
-                                                            'Dev Team',
-                                                            TagType.H4);
+    TargetType.SELF,
+    false,
+    'Dev Team',
+    TagType.H4);
 export const discordElementLink = new ElementLink('https://discord.gg/EJUJEBKgHJ',
-                                                  TargetType.BLANK)
+    TargetType.BLANK)
 
-export const currencyConverterElementLink = new TextElementLink('https://github.com/UWRF-Development-Team/Currency-Converter',
-                                                                TargetType.BLANK,
-                                                                false,
-                                                                'View Source Code',
-                                                                TagType.H6);
-export const gamesWebsiteElementLink = new TextElementLink('https://github.com/UWRF-Development-Team/Games-Website/tree/SpringV3',
-                                                           TargetType.BLANK,
-                                                           false,
-                                                           'View Source Code',
-                                                           TagType.H6);
-export const dadJokeApiElementLink = new TextElementLink('https://github.com/UWRF-Development-Team/Dad-Joke-API/tree/SpringAPI',
-                                                         TargetType.BLANK,
-                                                         false,
-                                                         'View Source Code',
-                                                         TagType.H6);
-export const devTeamWebsiteElementLink = new TextElementLink('https://github.com/UWRF-Development-Team/Dev-Team-Website',
-                                                             TargetType.BLANK,
-                                                             false,
-                                                             'View Source Code',
-                                                             TagType.H6);
-export const chessElementLink = new TextElementLink('https://github.com/UWRF-Development-Team/Chess/tree/main',
+export const replitElementLink = new TextElementLink("https://replit.com/join/hhnwrlizyo-theoliverlear",
                                                     TargetType.BLANK,
                                                     false,
-                                                    'View Source Code',
-                                                    TagType.H6);
+                                                    "Debug on Replit",
+                                                    TagType.H3);
+
+
+export const currencyConverterElementLink = new TextElementLink('https://github.com/UWRF-Development-Team/Currency-Converter',
+    TargetType.BLANK,
+    false,
+    'View Source Code',
+    TagType.H6);
+export const gamesWebsiteElementLink = new TextElementLink('https://github.com/UWRF-Development-Team/Games-Website/tree/SpringV3',
+    TargetType.BLANK,
+    false,
+    'View Source Code',
+    TagType.H6);
+export const dadJokeApiElementLink = new TextElementLink('https://github.com/UWRF-Development-Team/Dad-Joke-API/tree/SpringAPI',
+    TargetType.BLANK,
+    false,
+    'View Source Code',
+    TagType.H6);
+export const devTeamWebsiteElementLink = new TextElementLink('https://github.com/UWRF-Development-Team/Dev-Team-Website',
+    TargetType.BLANK,
+    false,
+    'View Source Code',
+    TagType.H6);
+export const chessElementLink = new TextElementLink('https://github.com/UWRF-Development-Team/Chess/tree/main',
+    TargetType.BLANK,
+    false,
+    'View Source Code',
+    TagType.H6);
 export const taskManagerElementLink = new TextElementLink('https://github.com/UWRF-Development-Team/Task-Manager',
-                                                          TargetType.BLANK,
-                                                          false,
-                                                          'View Source Code',
-                                                          TagType.H6);
+    TargetType.BLANK,
+    false,
+    'View Source Code',
+    TagType.H6);
 export const geoflagElementLink = new TextElementLink('https://github.com/UWRF-Development-Team/GeoFlag',
-                                                      TargetType.BLANK,
-                                                      false,
-                                                      'View Source Code',
-                                                      TagType.H6);
+    TargetType.BLANK,
+    false,
+    'View Source Code',
+    TagType.H6);
