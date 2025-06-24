0d972cf462612d093c442f50d49c3e02a8e001d2
theoliverlear <sigwartholiver2@gmail.com>
2025-06-24 17:26:43 -0500

    Add topic-bubble.component.ts to be an Angular component for the topic-bubble element in the Angular application

---
+++ angular/components/elements/topic-bubble/topic-bubble.component.ts
@@ -0,0 +1,13 @@
+// topic-bubble.component.ts 
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'topic-bubble',
+    templateUrl: './topic-bubble.component.html',
+    styleUrls: ['./topic-bubble.component.css']
+})
+export class TopicBubbleComponent {
+    constructor() {
+        
+    }
+}
